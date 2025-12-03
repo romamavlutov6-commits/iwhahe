3a7d575b471acbe4a1cfda1f8a95e57bf523efc5
Iwhahe <romamavlutov6@gmail.com>
2025-12-03 22:35:51 +0300

    я учюсь создавать файлики

---
+++ prac 18/task1/point1.java
@@ -0,0 +1,12 @@
+package task1;
+
+public class point1
+{
+    public static void exceptionDemo() {
+        System.out.println(2 / 0);
+    }
+
+    public void main(String[] args) {
+         point1.exceptionDemo();
+    }
+}
